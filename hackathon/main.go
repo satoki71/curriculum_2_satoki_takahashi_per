9466882a9684cfcb8e281cdfd1d8110ee3cb3b60
satoki71 <sakohiei71@icloud.com>
2022-11-16 20:19:37 +0900

    Modify main.go for CORS at givehandler

--- hackathon/main.go
+++ hackathon/main.go
@@ -174,8 +174,8 @@ func takeHandler(w http.ResponseWriter, r *http.Request) {
 func giveHandler(w http.ResponseWriter, r *http.Request) {
 	//w.Header().Set("Access-Control-Allow-Headers", "http://localhost:3000")
 	//w.Header().Set("Access-Control-Allow-Origin", "http://localhost:3000")
-	w.Header().Set("Access-Control-Allow-Headers", "*")
-	w.Header().Set("Access-Control-Allow-Origin", "*")
+	w.Header().Set("Access-Control-Allow-Headers", "http*")
+	w.Header().Set("Access-Control-Allow-Origin", "http*")
 	w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
 	w.Header().Set("Content-Type", "application/json")
 
