820cbe4ba54a4922b4261c316b544f3ffbf99cf4
satoki71 <sakohiei71@icloud.com>
2022-11-22 12:05:59 +0900

    Modify affiliation_search_usecase at UAffiliationSearch

--- hackathon/usecase/affiliation_search_usecase.go
+++ hackathon/usecase/affiliation_search_usecase.go
@@ -49,7 +49,7 @@ func UAffiliationSearch(userId string) (affiliation []model.AffiliationResForHTT
 	affiliation = make([]model.AffiliationResForHTTPGet, 0)
 	for rows.Next() {
 		var u model.AffiliationResForHTTPGet
-		if err := rows.Scan(&u.Id, &u.Name, &u.Name, &u.Number); err != nil {
+		if err := rows.Scan(&u.Id, &u.Name, &u.Number); err != nil {
 			log.Printf("fail: rows.Scan, %v\n", err)
 			if err := rows.Close(); err != nil { // 500を返して終了するが、その前にrowsのClose処理が必要
 				log.Printf("fail: rows.Close(), %v\n", err)
